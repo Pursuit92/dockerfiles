9895819739b789fdbe59d70f1f3921dbe77507cd
Josh Chase <jcjoshuachase@gmail.com>
2014-10-09 19:12:37 -0400

    whoops, forgot function

--- buildandpush.sh
+++ buildandpush.sh
@@ -2,6 +2,13 @@
 
 images=$(cat images)
 
+build_image(){
+	img=$(basename $(dirname $(pwd)))
+	tag=$(basename $(pwd))
+
+	docker build --tag jechas01/$img:$tag --force-rm .
+}
+
 for d in $images
 do
 	pushd $d
