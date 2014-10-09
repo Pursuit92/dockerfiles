0747e50d21e5518e3755cbd2f6d0917be6cc8a02
Josh Chase <jcjoshuachase@gmail.com>
2014-10-09 17:09:28 -0400

    updated build system to remove make dependecy

--- build.sh
+++ build.sh
@@ -2,9 +2,16 @@
 
 images=$(cat images)
 
+build_image(){
+	img=$(basename $(dirname $(pwd)))
+	tag=$(basename $(pwd))
+
+	docker build --tag jechas01/$img:$tag --force-rm .
+}
+
 for d in $images
 do
 	pushd $d
-	make
+	build_image
 	popd
 done
